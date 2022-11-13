9e164eb86c1403d89a4fda52f4da52d294577cbf
imtiajul-islam-d <imtiajul@hotmail.com>
2022-11-13 21:06:09 +0600

    footer text updated

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -34,7 +34,7 @@ const Footer = () => {
       </footer>
       <footer className="footer footer-center p-4 text-base-content">
         <div>
-          <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+          <p>Copyright © 2022 - All right Ltd</p>
         </div>
       </footer>
     </section>
